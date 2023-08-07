57fde965e7e6ae3b4ee57f84f4d604192e6a66df
mittani_s <hoshimi.tentai110@gmail.com>
2023-08-08 00:05:27 +0900

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Box } from "@mantine/core";
+import useSWR from "swr";
+
+export const ENDPOINT = "http://localhost:4000";
+
+const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
+
+function App() {
+    const { data, mutate } = useSWR("api/todos", fetcher);
+    return <Box>{JSON.stringify(data)}</Box>;
+}
+
+export default App;
